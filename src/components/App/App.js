193a221e805a0b5fba8817ad8efe6808e3574a33
achiverram28 <97288756+achiverram28@users.noreply.github.com>
2022-04-05 17:33:41 +0530

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Landing from './components/MainPage/Landing';
-import Imageslider from './components/MainPage/ImageSlider';
+import ImageSlider from './components/MainPage/ImageSlider';
+import { SliderData } from "./components/MainPage/SliderData";
 
 function App() {
   return (
   <div className="App">
-    <ImageSlider/>
+    <ImageSlider slides={SliderData}/>
       <Landing/>
     </div>
   );
