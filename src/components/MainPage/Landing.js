e2c3c1a126f752c62ac7ed3b7f32941076c8b787
achiverram28 <97288756+achiverram28@users.noreply.github.com>
2022-04-05 17:03:15 +0530

    Update Landing.js

--- src/components/MainPage/Landing.js
+++ src/components/MainPage/Landing.js
@@ -1,10 +1,19 @@
-import React from "react";
-import './Landing.css';
+import React, { useState, useEffect } from "react";
+import { SliderData } from "./components/MainPage/SliderData";
+import "./components/MainPage/Landing.css";
 
 const Landing = () => {
-    return(
-        <p>Landing Page</p>
-    );
-}
+  return (
+    <div className="text-style">
+      <p>
+        <h1 className="word-style">
+          WILD<span className="light">BEATS</span>
+        </h1>
+
+        <span class="below">Cultural Club Of IIIT Kottayam</span>
+      </p>
+    </div>
+  );
+};
 
 export default Landing;
