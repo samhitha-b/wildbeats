765643b09d8f79c8b54d475678c204c2263e2c28
Ganesh Nathan <85446331+Welf06@users.noreply.github.com>
2022-05-05 22:36:16 +0530

    added navbar to App.Js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Navbar from '../Navbar/Navbar';
 import Landing from '../MainPage/Landing';
 
 
 function App() {
   return (
   <div className="App">
+      <Navbar/>
       <Landing/>
     </div>
   );
